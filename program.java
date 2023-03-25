dfb4c011316a64efdda6043ef1bdcdd264a8297a
AnjaSamsom <anjavt14@gmail.com>
2023-03-24 22:31:32 -0400

    commit before I mess stuff up

--- program.java
+++ program.java
@@ -1,4 +1,7 @@
 import java.util.*;
+import java.util.Queue;
+import java.util.*;
+
 
 
 /**
@@ -27,10 +30,10 @@ public class program
    public static void main(String[]args)
    {
       int[] L1 = {1, 13, 14, 2, 25, 26};
-      int[] L2 = {6};
+      int[] L2 = {7,8,9,6};
    
       
-    int [][] arr = sort_and_count(L1);
+    sector arr = sort_and_count(L2);
     System.out.println("Inversion count: " + arr[0][0] + ". Sorted array: " + Arrays.toString(arr[1]));
 
 
@@ -38,9 +41,8 @@ public class program
       
    }
 
-   public static int[][] sort_and_count(int[] L)
+   public static sector sort_and_count(int[] L)
    {
-      int[][] arr = new int[2][L.length];
       if(L.length == 1)
       {
         arr[0][0] = 0;
@@ -48,18 +50,97 @@ public class program
         return arr;
       }
 
+      int [][] A = new int [2][L.length/2];
+      A[1] = Arrays.copyOfRange(L, 0, L.length/2);
+      int [][] B = new int [2][L.length/2];
+      B[1] = Arrays.copyOfRange(L, L.length/2, L.length);
 
-      int[] A = Arrays.copyOfRange(L, 0, L.length/2);
-      int[] B = Arrays.copyOfRange(L, L.length/2, L.length);
 
+      A = sort_and_count(A[1]);
+      B = sort_and_count(B[1]);
 
+      //System.out.println("A");
+      //System.out.println(Arrays.toString(A[1]));
 
-      
+  //    System.out.println("B");
+//      System.out.println(Arrays.toString(B[1]));
 
-      arr[0][0] = 0;
-      arr[1] = L;
+      int arrR [][] = merge_and_count(A[1],B[1]);
+
+      int ra = A[0][0];
+      int rb = A[0][0];
+      int r = arrR[0][0];
+
+      arr[0][0] = ra+rb+r;
+      arr[1] = arrR[1];
       
       return arr;
+   }
+
+   public static int[][] merge_and_count(int[] A, int[] B)
+   {
+      // make a queue for A and B
+
+      LinkedList<Integer> qA = new LinkedList<Integer>();
+      for(int num : A)
+      {
+         qA.add(num);
+      }
+
+      LinkedList<Integer> qB = new LinkedList<Integer>();
+      for(int num : B)
+      {
+         qB.add(num);
+      }      
+
+      int inv = 0;
+      int [][] arr  = new int[2][A.length + B.length];
+
+      LinkedList<Integer> R = new LinkedList<Integer>();
+
+      for(int i = 0; i<qA.size() && i<qB.size(); i++)
+      {
+        // System.out.println("compare");
+
+         System.out.println(qA.get(i));
+         System.out.println(qB.get(i));
+         System.out.println();
+
+         if(qA.get(i) < qB.get(i))
+         {
+            System.out.println("added " + qA.get(i));
+            R.add(qA.remove(i));
+
+
+         }
+         else if( qA.get(i) > qB.get(i))
+         {
+            System.out.println("added " + qB.get(i));
+            R.add(qB.remove(i));
+            inv ++;
+         }
+      }
+
+      arr[0][0] = inv;
+
+
+
+      for(int unit : R)
+      {
+
+      }
+
+
+
+      System.out.println(Arrays(R));
+
+
+
+
+      return arr;
+
+
+
    }
 
    
---
+++ sector.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class sector {
+
+
+    public int inversions;
+    public ArrayList<Integer> nums = new ArrayList<Integer>(); // Create an ArrayList object
+
+    public sector(ArrayList<String> nums, int inversions)
+    {
+        this.inversions = inversions;
+        this.nums = nums;
+    }
+
+}
